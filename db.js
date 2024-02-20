28ba019ff18e703565dff8f0f4a3a892baef2d6a
SamFehl <sjfehl@pointpark.edu>
2024-02-20 18:29:15 -0500

    upload required files for module 3 api

--- db.js
+++ db.js
@@ -13,10 +13,10 @@ const pool = new Pool({
     //Remote Connection
     /*
     user: "sjfehl",
-    host: "localhost",
-    password: "@gL22031621384",
-    database: "users",
-    port: "5503",
+    host: "dpg-cna2s6ta73kc73elif7g-a",
+    password: "0Gc3BZXLhJT81tMpKKr3MsyD2slFjlPi",
+    database: "userspost",
+    port: "5432",
     */
 });
 
