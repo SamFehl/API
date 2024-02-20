ef888627261c57249be434d31d5f2fe32fd6f97a
SamFehl <sjfehl@pointpark.edu>
2024-02-19 23:07:37 -0500

    upload edits for db remote connection

--- db.js
+++ db.js
@@ -2,13 +2,22 @@ const pool = require('pg');
 
 const pool = new pool({
     //Local Connection
+    ///*
     user: "sjfehl",
     host: "localhost",
     password: "@gL22031621384",
     database: "users",
     port: "5503",
+    //*/
 
-    //
+    //Remote Connection
+    /*
+    user: "sjfehl",
+    host: "localhost",
+    password: "@gL22031621384",
+    database: "users",
+    port: "5503",
+    */
 });
 
 module.exports = pool;
